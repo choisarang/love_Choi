7a147422f8ff93ed77d55b24c5425613ef410c6e
choi sarang <tkfkd2fkrh@gmail.com>
2019-10-22 17:58:58 +0900

    10.22

--- e_example/js/src/jquery/spigen_temp_pc/spigen_main_content_01.js
+++ e_example/js/src/jquery/spigen_temp_pc/spigen_main_content_01.js
@@ -14,10 +14,53 @@
     btn.addClass('action');
     btnParent.siblings('dt').children('button').removeClass('action');
 
+    /*
+    // step1
     parNext.addClass('action');
     parNext.siblings('dd').removeClass('action');
+    */
 
+    //step2
+    parNext.stop().fadeIn();
+    parNext.siblings('dd').stop().fadeOut();
+    // action 클래스이름으로 구분만 할 수 있도록 처리하고, css에서 동작하지 않도록 처리
   });
+
+  // ===========================================================================
+    const bmText = '\
+      <div class="album">\
+        <a href="#">\
+          <h4>blog</h4>\
+          <p class="con"></p>\
+          <p class="date"></p>\
+        </a>\
+      </div>';
+  
+    const mediaList = [
+      {text:'[youtube] 아마존으로 2600억 매출', date:'2019.10.22'},
+      {text:'슈피겐코리아 미국 아마존 독점유통', date:'2019.10.20'},
+      {text:'[youtube]\'취업비자\'기업탐방', date:'2019.10.10'}
+      ];
+
+      // mediaList[0].text => 1번째 가져오기
+
+
+
+
+    // const blogList = ;
+
+    const media = conArea.find('.media');
+    // for(최초의 값; 조건비교; 값의 증감;){}
+    for(let i = 0; i < mediaList.length; i++){
+      media.append(bmText);
+      let myNth = media.children('.album').eq(i);
+      myNth.find('h4').text('media');
+      myNth.find('.con').text(mediaList[i].text);
+      myNth.find('.date').text(mediaList[i].date);
+
+    }
+
+  // ===========================================================================
   
   
 
